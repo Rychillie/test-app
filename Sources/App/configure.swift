542e40326a90e83cf3e09ba02cd19a8e1ac8f535
Rychillie <rychillie@rychillie.net>
2025-02-24 20:52:45 -0300

    Testing

--- Sources/App/configure.swift
+++ Sources/App/configure.swift
@@ -5,26 +5,22 @@ import Vapor
 // configures your application
 public func configure(_ app: Application) async throws {
     // uncomment to serve files from /Public folder
-    let pathToPublic = URL(fileURLWithPath: #filePath)
-        .deletingLastPathComponent()
-        .appendingPathComponent("Public")
-        .relativePath
+    let pathToPublic = "\(app.directory.workingDirectory)/Sources/App/Public"
     app.middleware.use(FileMiddleware(publicDirectory: pathToPublic))
     
-    let pathToViews = URL(fileURLWithPath: #filePath)
-        .deletingLastPathComponent()
-        .appendingPathComponent("Views")
-        .relativePath
-    app.leaf.sources = LeafSources.singleSource(
-        NIOLeafFiles(
-            fileio: app.fileio,
-            limits: .default,
-            sandboxDirectory: pathToViews,
-            viewDirectory: pathToViews
-        )
-    )
-
+    let pathToViews = "\(app.directory.workingDirectory)/Sources/App/Views"
+    app.logger.info("Configuring Leaf with views directory: \(pathToViews)")
+    
+    app.leaf.configuration.rootDirectory = pathToViews
     app.views.use(.leaf)
+    
+    // Log the contents of the views directory
+    let fileManager = FileManager.default
+    if let files = try? fileManager.contentsOfDirectory(atPath: pathToViews) {
+        app.logger.info("Files in views directory: \(files)")
+    } else {
+        app.logger.error("Failed to read contents of views directory: \(pathToViews)")
+    }
 
     // register routes
     try routes(app)
